24daf98e345cb6c89918eaf79081e431a72a0200
Adolfo Fuentes <adolfo_fuentes@outlook.com>
2023-03-04 13:54:14 +0100

    Secure subscription service

--- subscription_service/index.js
+++ subscription_service/index.js
@@ -1,5 +1,23 @@
-const express = require("express");
+require('dotenv').config({path: __dirname + '/../.env'});
+
+const express = require('express');
+const cors = require('cors');
+
+const bodyParser = require('body-parser');
 const app = express();
-app.listen(3002, () => {
-  console.log("Email server is running on localhost:3002...");
+const port = process.env.SUBS_SERVER_PORT;
+const middleware = require('../security/middleware');
+const service = require('../security/service');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log(`Subscription server is running on localhost:${port}...`);
+});
+
+app.post('/auth', service.authentication);
+
+app.post('/create_subscription', middleware.isAuthenticated, (req, res) => {
+  res.send();
 });
